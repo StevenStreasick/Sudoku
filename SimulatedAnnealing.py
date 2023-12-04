7fa6ce4536551caa65f1da4bf4476c68c9459dcc
Steven Streasick <StevenStreasick@gmail.com>
2023-12-03 19:46:47 -0500

    Delete SimulatedAnnealing.py

--- SimulatedAnnealing.py
+++
@@ -1,8 +0,0 @@
-#Fill in grid based on valid sub grid. Do not care about rows/col 
-#restrictions
-
-#Cost function measures the # of row/column violations. 
-
-#Starting temp = standard deviation of the cost of 200 starting states. 
-
-# #of iterations per temp = 
